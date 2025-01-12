ef76be35943f5bd3430f518f86eca402dd517771
Benj1907 <benjamin.gauron@gmail.com>
2025-01-12 12:23:38 +0000

    new functions

--- module.py
+++ module.py
@@ -1,4 +1,25 @@
-if __name__ =="__main__" :
-    print("module.py is being run directly")
-else:
-    print("module.py is being imported into another module")
+#!/usr/bin/env python3 
+
+__counter = 0
+
+def suml(list):
+    global __counter
+    __counter += 1
+    sum = 0
+    for element in list:
+        sum += element
+    return sum
+
+def prodl(list):
+    global __counter    
+    __counter += 1
+    prod = 1
+    for element in list:
+        prod *= element
+    return prod
+
+if __name__ == "__main__":
+    print("I prefer to be a module, but I can do some tests for you")
+    l = [i+1 for i in range(5)]
+    print(suml(l) == 15)
+    print(prodl(l) == 120)
